52c5f4485d9c6f2542781966843ae354ffc7a120
Abhinav Girdhar <girdhaab@sheridancollege.ca>
2020-05-28 00:16:21 -0400

    Adding Part Time Feature

---
+++ src/studentlist/PartTImeStudent.java
@@ -0,0 +1,28 @@
+package studentlist;
+
+/**
+ * This class gives info regarding parttime student
+ * @author Abhinav
+ */
+public class PartTImeStudent {
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+
+}
